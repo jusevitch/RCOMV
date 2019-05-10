df7431ca09756e630d1517c167ac9b7766adc62b
James Usevitch <usevitch@umich.edu>
2019-05-10 12:23:58 -0400

    msrpa_auto_leader.cpp tested and working.

---
+++ rcomv_r1/launch/IO_launch/test_auto_leader.py
@@ -0,0 +1,54 @@
+# Launch file script for n agents with IO_collision avoidance and msrpa
+# This is for hardware with physical obstacles (i.e. gazebo = 0)
+
+import sys
+## Append easylaunch directory to path
+# sys.path.append('/home/dasc/Downloads/MSR/launch/easylaunch')
+sys.path.append('/media/james/Data/code2/easylaunch')
+
+from math import pi, cos, sin
+from random import randint, sample
+import easylaunch as el
+import random
+
+
+
+# Launchfile object
+
+launch = el.launchFile()
+
+
+# Configurations
+
+flattened_trajectory_list = []
+flattened_formation_list = []
+time_duration_list = [5.0, 5.0, 5.0, 5.0]
+
+
+for ii in range(4):
+    flattened_trajectory_list += [7.0] + [random.random() for jj in range(7)]
+    flattened_formation_list += [2.0] + [random.random() for kk in range(2)]
+
+
+
+
+# Nodes
+
+launch.node = []
+
+node_1 = el.node(name="auto_leader", pkg="rcomv_r1", type="msrpa_auto_leader", output="screen", launch_prefix="xterm -e gdb -ex run --args")
+node_1.param = {
+    "number_of_configurations": "4",
+}
+node_1.rosparam ={
+    "trajectory_types": "['circular', 'square', 'circular', 'square']",
+    "flattened_trajectory_list": str(flattened_trajectory_list),
+    "flattened_formation_list": str(flattened_formation_list),
+    "time_duration_list": str(time_duration_list)
+}
+
+launch.node = [node_1]
+
+# Write
+launchfile_name = "./test_auto_leader.launch"
+launch.write(filename=launchfile_name)
--- rcomv_r1/src/msrpa_auto_leader.cpp
+++ rcomv_r1/src/msrpa_auto_leader.cpp
@@ -20,7 +20,7 @@ class Auto_Leader
     std::vector<std::string> trajectory_types;
     std::vector<double> flattened_trajectory_list; // 1D array with all trajectory vectors
     std::vector<double> flattened_formation_list;  // 1D array with all formation vectors
-    std::vector<double> time_list;
+    std::vector<double> time_duration_list;
 
     // Publishers
     ros::Publisher pub;
@@ -34,15 +34,15 @@ class Auto_Leader
     std::deque<std::deque<double> > master_trajectory_list;
     std::deque<std::deque<double> > master_formation_list;
     int configuration_counter;
-    double t, t0; // Initial time
+    double current_time, t0; // Initial time
     double prev_switching_time, next_switching_time;
 
     // Functions
     void timerCallback(const ros::TimerEvent& event);
-    std::deque<std::deque<double> > parse_trajectory_list(std::deque<double> d);
-    std::deque<std::deque<double> >  parse_formation_list(std::deque<double> d);
-    std::deque<double> vec2deque(const std::vector<double> &vec);
-    std::vector<double> deque2vec(const std::deque<double> &deq);
+    std::deque<std::deque<double> > parse_trajectory_list(std::deque<double> d, int size);
+    std::deque<std::deque<double> >  parse_formation_list(std::deque<double> d, int size);
+    std::deque<double> vec2deque(const std::vector<double> &vec, int size);
+    std::vector<double> deque2vec(const std::deque<double> &deq, int size);
 
 
 
@@ -58,11 +58,32 @@ Auto_Leader::Auto_Leader()
     nh_private_.param<std::vector<std::string> >("trajectory_types", trajectory_types, std::vector<std::string>());
     nh_private_.param<std::vector<double> >("flattened_trajectory_list", flattened_trajectory_list, std::vector<double>());
     nh_private_.param<std::vector<double> >("flattened_formation_list", flattened_formation_list, std::vector<double>());
-    nh_private_.param<std::vector<double> >("time_list", time_list, std::vector<double>()); // Length must be equal to number_of_configurations
+    nh_private_.param<std::vector<double> >("time_duration_list", time_duration_list, std::vector<double>()); // Length must be equal to number_of_configurations
 
     // Parse trajectories and Formations
-    master_trajectory_list = parse_trajectory_list(vec2deque(flattened_trajectory_list));
-    master_formation_list = parse_trajectory_list(vec2deque(flattened_formation_list));
+    master_trajectory_list = parse_trajectory_list(vec2deque(flattened_trajectory_list, flattened_trajectory_list.size()), flattened_trajectory_list.size());
+    master_formation_list = parse_trajectory_list(vec2deque(flattened_formation_list, flattened_formation_list.size()), flattened_formation_list.size());
+
+    for(int ii = 0; ii < master_trajectory_list.size(); ii++)
+    {
+        for(int jj = 0; jj < master_trajectory_list[ii].size(); jj++)
+        {
+            ROS_INFO("mtraject[ii][jj]: %lf", master_trajectory_list[ii][jj]);
+        }
+        ROS_INFO("\n");
+        
+    }
+    ROS_INFO("\n\n");
+
+    for(int ii = 0; ii < master_formation_list.size(); ii++)
+    {
+        for(int jj = 0; jj < master_formation_list[ii].size(); jj++)
+        {
+            ROS_INFO("mform[ii][jj]: %lf", master_formation_list[ii][jj]);
+        }
+        ROS_INFO("\n");
+    }
+    
 
     // Publishers
     pub = nh.advertise<rcomv_r1::MSRPA>("/leader_cmd", 1);
@@ -74,9 +95,10 @@ Auto_Leader::Auto_Leader()
     pubTimer = nh.createTimer(ros::Duration(0.05), &Auto_Leader::timerCallback, this);
 
     configuration_counter = 0;
+    ros::spinOnce();
     t0 = ros::Time::now().toSec();
     prev_switching_time = t0;
-    next_switching_time = t0 + time_list[0];
+    next_switching_time = t0 + time_duration_list[0];
 
 }
 
@@ -93,85 +115,123 @@ Auto_Leader::~Auto_Leader()
 
 void Auto_Leader::timerCallback(const ros::TimerEvent& event)
 {
-    t = ros::Time::now().toSec() - t0;
-    if (t > next_switching_time){
-        configuration_counter++;
+
+    current_time = ros::Time::now().toSec();
+    if (current_time > next_switching_time){
+        configuration_counter = (configuration_counter + 1) % number_of_configurations;
         prev_switching_time = next_switching_time;
-        next_switching_time = t0 + time_list[configuration_counter % number_of_configurations];
+        next_switching_time += time_duration_list[configuration_counter];
     }
 
     rcomv_r1::MSRPA msg;
     msg.type = trajectory_types[configuration_counter];
-    msg.trajectory = deque2vec(master_trajectory_list[configuration_counter]);
-    msg.formation = deque2vec(master_formation_list[configuration_counter]);
+    msg.trajectory = deque2vec(master_trajectory_list[configuration_counter], master_trajectory_list[configuration_counter].size());
+    msg.formation = deque2vec(master_formation_list[configuration_counter], master_formation_list[configuration_counter].size());
+
+    // ROS_INFO("trajectory: [%lf, %lf, %lf]", msg.trajectory[0], msg.trajectory[1], msg.trajectory[2]);
 
     pub.publish(msg);
 
 }
 
-std::deque<std::deque<double> > Auto_Leader::parse_trajectory_list(std::deque<double> d)
+std::deque<std::deque<double> > Auto_Leader::parse_trajectory_list(std::deque<double> d, int size)
 {
     // Format of the trajectory list:
     // Each trajectory t_i is formatted as [length_of_traj param1 param2 ...]
     // flattened_trajectory_list is formatted as [t0 t1 ...]
     
+    int idx = 0;
+
     std::deque<std::deque<double> > out_deque;
+    ROS_INFO("size: %d", size);
     for (int ii = 0; ii < number_of_configurations; ii++)
     {
-        int temp_num_entries = static_cast<int>(d[0]);
+        ROS_INFO("\n temp_num_entries before: %lf", d[idx]);
+        int temp_num_entries = static_cast<int>(d[idx]);
+        ROS_INFO("\ntemp_num_entries: %d", temp_num_entries);
+        idx += 1;
+        ROS_INFO("idx: %d", idx);
         out_deque.push_back(std::deque<double>(temp_num_entries));
-        for (int jj = 1; jj <= temp_num_entries; jj++)
+        for (int jj = idx; jj < idx + temp_num_entries; jj++)
         {
-            out_deque[ii][jj - 1] = d[jj];
+            out_deque[ii][jj - idx] = d[jj];
+            ROS_INFO("jj: %d", jj);
+            ROS_INFO("entry: %lf", d[jj]);
         }
-        d.erase(d.begin(), d.begin() + temp_num_entries + 1);
+        idx += temp_num_entries;
     }
 
-    if (d.size() > 0)
-    {
-        ROS_INFO("WARNING: Not all entries turned into trajectories.");
-    }
+    // if (d.size() > 0)
+    // {
+    //     ROS_INFO("d.size(): %lu", d.size());
+    //     for(int ii = 0; ii < d.size(); ii++)
+    //     {
+    //         ROS_INFO("d[ii]: %lf", d[ii]);
+    //     }
+        
+    //     ROS_INFO("WARNING: Not all entries turned into trajectories.");
+    // }
 
+    ROS_INFO("\n");
     return out_deque;
 }
 
-std::deque<std::deque<double> > Auto_Leader::parse_formation_list(std::deque<double> d)
+std::deque<std::deque<double> > Auto_Leader::parse_formation_list(std::deque<double> d, int size)
 {
+
+    int idx = 0;
+
     std::deque<std::deque<double> > out_deque;
+    ROS_INFO("size: %d", size);
     for (int ii = 0; ii < number_of_configurations; ii++)
     {
-        int temp_num_entries = static_cast<int>(d[0]);
+        ROS_INFO("\n temp_num_entries before: %lf", d[idx]);
+        int temp_num_entries = static_cast<int>(d[idx]);
+        ROS_INFO("temp_num_entries after: %d", temp_num_entries);
+        idx += 1;
+        ROS_INFO("idx: %d", idx);
         out_deque.push_back(std::deque<double>(temp_num_entries));
-        for (int jj = 1; jj <= temp_num_entries; jj++)
+        for (int jj = idx; jj < idx + temp_num_entries; jj++)
         {
-            out_deque[ii][jj - 1] = d[jj];
+            out_deque[ii][jj - idx] = d[jj];
+            ROS_INFO("jj: %d", jj);
+            ROS_INFO("entry: %lf", d[jj]);
         }
-        d.erase(d.begin(), d.begin() + temp_num_entries + 1);
+        idx += temp_num_entries;
     }
 
-    if (d.size() > 0)
-    {
-        ROS_INFO("WARNING: Not all entries turned into trajectories.");
-    }
+    // if (d.size() > 0)
+    // {
+    //     ROS_INFO("d.size(): %lu", d.size());
+    //     for(int ii = 0; ii < d.size(); ii++)
+    //     {
+    //         ROS_INFO("d[ii]: %lf", d[ii]);
+    //     }
+        
+    //     ROS_INFO("WARNING: Not all entries turned into trajectories.");
+    // }
 
+    ROS_INFO("\n");
     return out_deque;
 }
 
-std::deque<double> Auto_Leader::vec2deque(const std::vector<double> &vec)
+std::deque<double> Auto_Leader::vec2deque(const std::vector<double> &vec, int size)
 {
-    std::deque<double> out_deque;
-    for (int ii = 0; ii < vec.size(); ii++)
+    std::deque<double> out_deque(size);
+    for (int ii = 0; ii < size; ii++)
     {
         out_deque[ii] = vec[ii];
     }
     return out_deque;
 }
 
-std::vector<double> Auto_Leader::deque2vec(const std::deque<double> &deq){
-    std::vector<double> out_vector;
-    for (int ii = 0; ii < deq.size(); ii++)
+std::vector<double> Auto_Leader::deque2vec(const std::deque<double> &deq, int size){
+    std::vector<double> out_vector(size);
+    for (int ii = 0; ii < size; ii++)
     {
+        // ROS_INFO("deq[ii]: %lf", deq[ii]);
         out_vector[ii] = deq[ii];
+        // ROS_INFO("outvector[ii]: %lf", out_vector[ii]);
     }
     return out_vector;
 }
